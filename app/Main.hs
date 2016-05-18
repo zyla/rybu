105481683d5d58636a61e05ef40032380bbeb553
zyla <zyla@prati.pl>
2016-05-18 09:08:35 +0000

    Add working main

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,43 @@
-module Main (module Dedan) where
+module Main where
 
-import Dedan
+import Control.Monad
+import qualified Data.Map as M
+import Imp
+import ImpParser (parseModel)
+import CompileProc
+
+main = getContents >>= processModelFile
+
+processModelFile file =
+  case parseModel "" file of
+
+    Left err ->
+      putStrLn $ "Syntax error: " ++ show err
+
+    Right Model{model_servers=servers, model_procs=procs} -> do
+
+      forM_ servers $ \srv -> do
+        putStrLn $ "server " ++ server_name srv
+        case compileTransitions srv of
+          Left err -> putStrLn $ "Error: " ++ show err
+          Right transitions -> mapM_ print transitions
+
+      forM_ procs $ \proc -> do
+        putStrLn $ "process " ++ show proc
+        putStrLn $ show (desugar $ process_stmt proc)
+        let ((first, last), g) = compile $ desugar $ process_stmt proc
+        forM_ (M.toList g) $ \(state, content) -> do
+            putStr (if state == first then "*" else " ")
+            print (state, content)
+        putStrLn "---"
+        case totallyCompileProcess (process_stmt proc) of
+            Left err -> print err
+
+            Right compiled -> do
+                putStrLn $ "initial: {"
+                    ++ message_server (cp_initialMessage compiled) ++ "."
+                    ++ message_msg (cp_initialMessage compiled) ++ ", "
+                    ++ cp_initialState compiled ++ "}"
+
+                forM_ (cp_transitions compiled) $ \(state, in_msg, Message s m, outState) ->
+                    putStrLn $ "{" ++ state ++ ", " ++ in_msg ++ "} -> {" ++ s ++ "." ++ m ++ ", " ++ outState ++ "}"
--- src/ImpParser.hs
+++ src/ImpParser.hs
@@ -10,7 +10,6 @@ import qualified Data.Set as S
 import qualified Data.Map as M
 
 import Imp
-import CompileProc
 
 T.TokenParser {..} = T.makeTokenParser L.haskellDef
     { T.reservedNames = [ "server", "process", "var", "loop", "match" ] }
@@ -93,39 +92,4 @@ matchCase = (,)
   
 message = Message <$> identifier <*> (reservedOp "." *> identifier <* parens (pure ()))
 
-
-main = getContents >>= processModelFile
-
-processModelFile file =
-  case parse model "" file of
-
-    Left err ->
-      putStrLn $ "Syntax error: " ++ show err
-
-    Right Model{model_servers=servers, model_procs=procs} -> do
-
-      forM_ servers $ \srv -> do
-        putStrLn $ "server " ++ server_name srv
-        case compileTransitions srv of
-          Left err -> putStrLn $ "Error: " ++ show err
-          Right transitions -> mapM_ print transitions
-
-      forM_ procs $ \proc -> do
-        putStrLn $ "process " ++ show proc
-        putStrLn $ show (desugar $ process_stmt proc)
-        let ((first, last), g) = compile $ desugar $ process_stmt proc
-        forM_ (M.toList g) $ \(state, content) -> do
-            putStr (if state == first then "*" else " ")
-            print (state, content)
-        putStrLn "---"
-        case totallyCompileProcess (process_stmt proc) of
-            Left err -> print err
-
-            Right compiled -> do
-                putStrLn $ "initial: {"
-                    ++ message_server (cp_initialMessage compiled) ++ "."
-                    ++ message_msg (cp_initialMessage compiled) ++ ", "
-                    ++ cp_initialState compiled ++ "}"
-
-                forM_ (cp_transitions compiled) $ \(state, in_msg, Message s m, outState) ->
-                    putStrLn $ "{" ++ state ++ ", " ++ in_msg ++ "} -> {" ++ s ++ "." ++ m ++ ", " ++ outState ++ "}"
+parseModel = parse model
