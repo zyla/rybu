cda703be6831bfd0922e625f8f077d0cdd250ee6
zyla <zyla@prati.pl>
2016-05-18 19:48:27 +0000

    Output servers for processes

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -11,21 +11,14 @@ import CompileProc
 generateDedan :: Model -> String
 generateDedan Model{..} = execWriter $ do
     let procNames = map process_name model_procs
+        Right compiledProcs = mapM (\(Process name stmt) -> (,) name <$> totallyCompileProcess stmt) model_procs
 
     forM_ model_servers $ \server@Server{..} -> do
-        tells ["server: ", server_name, " (servers "]
-        tell $ intercalate ", " (map procServerName procNames)
-        tell "; agents "
-        tell $ intercalate ", " (map procAgentName procNames)
-        tellLn "),"
-
-        tell "services {"
-        tell $ intercalate ", " (nub $ map t_name server_transitions)
-        tellLn "},"
-
-        tell "states {"
-        tell $ intercalate ", " (map (encodeState . M.toList) $ allStates $ M.toList server_vars)
-        tellLn "},"
+        serverHeader server_name
+            (map procServerName procNames)
+            (map procAgentName procNames)
+            (nub $ map t_name server_transitions)
+            (map (encodeState . M.toList) $ allStates $ M.toList server_vars)
 
         tellLn "actions {"
 
@@ -38,7 +31,40 @@ generateDedan Model{..} = execWriter $ do
                 in tells ["  {", agent, ".", server_name, ".", in_msg, ", ", server_name, ".", in_state, "} -> {"
                          , agent, ".", server, ".", out_msg, ", ", server_name, ".", out_state, "},\n"]
 
-        tellLn "};"
+        tellLn "};\n"
+
+    forM_ compiledProcs $ \(name, CompiledProc{..}) -> do
+        serverHeader (procServerName name)
+            (map siFormalParam model_serverInstances)
+            [procAgentName name]
+            cp_services
+            cp_states
+        tellLn "actions {"
+
+        forM_ cp_transitions $ \(in_state, in_msg, Message out_server out_msg, out_state) ->
+            let agent = procAgentName name
+                server = procServerName name
+            in tells ["  {", agent, ".", server, ".", in_msg, ", ", server, ".", in_state, "} -> {"
+                     , agent, ".", out_server, ".", out_msg, ", ", server, ".", out_state, "},\n"]
+
+        tellLn "};\n"
+
+siFormalParam ServerInstance{si_name=name, si_serverType=typ} = name ++ ": " ++ typ
+
+serverHeader serverName servers agents services states = do
+    tells ["server: ", serverName, " (servers "]
+    tell $ intercalate ", " servers
+    tell "; agents "
+    tell $ intercalate ", " agents
+    tellLn "),"
+
+    tell "services {"
+    tell $ intercalate ", " services
+    tellLn "},"
+
+    tell "states {"
+    tell $ intercalate ", " states
+    tellLn "},"
 
 procServerName = ("S_"++)
 procAgentName = ("A_"++)
