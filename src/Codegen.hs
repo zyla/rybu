zyla/rybu

commit 56608c741f36387207f3ede6dbe15accfdb22ffb

Only generate actions in server for processes that use the server; fixes Dedan's undefined continuation error

src/Codegen.hs
@@ -1,6 +1,7 @@
 module Codegen where
 
 import Control.Monad.Writer
+import qualified Control.Monad.State as MS
 import qualified Data.Map as M
 import Data.List (intercalate, nub)
 
@@ -21,7 +22,8 @@ generateDedan Model{..} = execWriterT $ do
     globalEnv <- lift $ foldM evalConst M.empty model_constants
 
     compiledProcs <- lift $ mapM (compileProcess globalEnv) model_procs
-    compiledServers <- lift $ mapM (compileServer globalEnv) model_servers
+    let serversUsage = serversUsageFromProcs model_serverInstances compiledProcs
+    compiledServers <- lift $ mapM (compileServer globalEnv serversUsage) model_servers
 
     forM_ compiledServers $ \server@CompiledServer{..} -> do
         serverHeader cs_name
@@ -32,7 +34,8 @@ generateDedan Model{..} = execWriterT $ do
 
         tellLn "actions {"
 
-        forM_ procNames $ \procName ->
+        -- TODO(hator): warn if cs_usedBy is 0, means server is not used by any process
+        forM_ cs_usedBy $ \procName ->
             forM_ cs_actions $ \ServerAction
                     {sa_inMessage=in_msg, sa_inState=in_state, sa_outMessage=out_msg, sa_outState=out_state} ->
                 let agent = procAgentName procName
@@ -114,3 +117,22 @@ tells = tell . concat
 
 tellLn :: Monad m => String -> WriterT String m ()
 tellLn str = tell str >> tell "\n"
+
+serversUsageFromProcs :: [ServerInstance] -> [CompiledProc] -> ServersUsage
+serversUsageFromProcs instances procs = MS.execState (forM_ procs collectServers) M.empty
+    where
+        collectServers :: CompiledProc -> MS.State ServersUsage ()
+        collectServers CompiledProc{..} = forM_ cp_usedServersInstances $ addServer cp_name . serverNameFromInstance
+
+        addServer :: ProcessName -> ServerName -> MS.State ServersUsage ()
+        addServer procN serverN = MS.modify $ M.insertWith (++) serverN [procN]
+
+        serverNameFromInstance :: ServerInstanceName -> ServerName
+        serverNameFromInstance name =
+            maybe "" id $ si_serverType <$> findServerInstance instances name
+
+        findServerInstance :: [ServerInstance] -> ServerInstanceName -> Maybe ServerInstance
+        findServerInstance [] name = Nothing
+        findServerInstance (i:is) name = if (si_name i) == name
+                                            then Just i
+                                            else findServerInstance is name

src/AST.hs
@@ -12,9 +12,10 @@ data Model = Model
     } deriving (Show)
 
 -- Server
+type ServerName = Symbol
 
 data Server = Server
-    { server_name :: Symbol
+    { server_name :: ServerName
     , server_vars :: [(Symbol, TypeExpr)]
     , server_transitions :: [Transition]
     } deriving (Show)
@@ -72,20 +73,23 @@ data BinOp = Plus | Minus | Modulo deriving (Show)
 
 data Value = Sym Symbol | Int Integer | Arr [Value] deriving (Eq, Show)
 
+type ServerInstanceName = Symbol
+
 data ServerInstance = ServerInstance
-    { si_name :: Symbol
-    , si_serverType :: Symbol
+    { si_name :: ServerInstanceName
+    , si_serverType :: ServerName
     , si_initialState :: [(Symbol, Expr)]
     } deriving (Show)
 
 -- Process
+type ProcessName = Symbol
 
 data Process = Process
-    { process_name :: Symbol
+    { process_name :: ProcessName
     , process_stmt :: Statement
     } deriving (Show)
 
 data Statement = Skip | Loop Statement | Block [Statement] | Msg Message | Match Message [(Symbol, Statement)]
     deriving (Show)
 
-data Message = Message { message_server :: Symbol, message_msg :: Symbol, message_params :: [Expr] } deriving (Show)
+data Message = Message { message_server :: ServerName, message_msg :: Symbol, message_params :: [Expr] } deriving (Show)

src/CompileProc.hs
@@ -11,10 +11,10 @@ import Err
 import Eval
 
 data CompiledProc = CompiledProc
-    { cp_name :: Symbol
+    { cp_name :: ProcessName
     , cp_states :: [Symbol]
     , cp_services :: [Symbol]
-    , cp_usedServers :: [Symbol]
+    , cp_usedServersInstances :: [ServerInstanceName]
     , cp_initialState :: Symbol
     , cp_initialMessage :: Message
     , cp_transitions :: [(Symbol, Symbol, Symbol, Symbol, Symbol)]
@@ -38,7 +38,7 @@ compileProcess globalEnv (Process name stmt) = withContext ("in process " ++ sho
         { cp_name = name
         , cp_states = dedup $ map (encodeStateId cfg . t_state) ts
         , cp_services = dedup $ map t_in_message ts
-        , cp_usedServers = dedup $ map (message_server . snd . t_cont) ts
+        , cp_usedServersInstances = dedup $ map (message_server . snd . t_cont) ts
         , cp_initialState = encodeStateId cfg initialState
         , cp_initialMessage = initialMessage
         , cp_transitions = encodedTs

src/CompileServer.hs
@@ -1,6 +1,7 @@
 module CompileServer where
 
 import Control.Monad
+import qualified Control.Monad.State as MS
 import Data.Maybe (fromMaybe)
 import Data.List (intercalate, nub)
 import qualified Data.Set as S
@@ -11,10 +12,11 @@ import Err
 import Eval
 
 data CompiledServer = CompiledServer
-    { cs_name :: Symbol
+    { cs_name :: ServerName
     , cs_states :: [Symbol]
     , cs_services :: [Symbol]
     , cs_actions :: [ServerAction]
+    , cs_usedBy :: [ProcessName]
     } deriving (Eq, Show)
 
 data ServerAction = ServerAction
@@ -24,8 +26,11 @@ data ServerAction = ServerAction
     , sa_outState :: Symbol
     } deriving (Eq, Show)
 
-compileServer :: Env -> Server -> EM CompiledServer
-compileServer env server@Server{..} = withContext ("in server " ++ server_name) $ do
+type ServersUsage = M.Map ServerName [ProcessName]
+
+compileServer :: Env -> ServersUsage -> Server -> EM CompiledServer
+compileServer env serversUsage server@Server{..} =
+  withContext ("in server " ++ server_name) $ do
     typeEnv <- fmap M.fromList $ forM server_vars $ \(name, typeE) ->
         withContext ("in type of var " ++ name) $
             (,) name <$> evalType env typeE
@@ -84,7 +89,6 @@ compileServer env server@Server{..} = withContext ("in server " ++ server_name) 
                             , sa_outMessage = outSignal
                             , sa_outState = encode (M.union updates state)
                             }
-                    
 
     actions <- concat <$> mapM compileTransition server_transitions
 
@@ -93,7 +97,10 @@ compileServer env server@Server{..} = withContext ("in server " ++ server_name) 
         , cs_states = map encodeState (allStates $ M.toList typeEnv)
         , cs_services = nub (map sa_inMessage actions)
         , cs_actions = actions
+        , cs_usedBy = getUsedBy serversUsage server_name
         }
+    where
+       getUsedBy serversUsage server_name = maybe [] id $ M.lookup server_name serversUsage
 
 listSet :: Int -> a -> [a] -> [a]
 listSet 0 x (_:xs) = x:xs

commit 3771e9f4d108813b95e3d4b3ebeb929644ecc171

Fix generating duplicate actions for servers (bug introduced in 56608c74)

src/Codegen.hs
@@ -3,6 +3,7 @@ module Codegen where
 import Control.Monad.Writer
 import qualified Control.Monad.State as MS
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.List (intercalate, nub)
 
 import AST
@@ -119,13 +120,14 @@ tellLn :: Monad m => String -> WriterT String m ()
 tellLn str = tell str >> tell "\n"
 
 serversUsageFromProcs :: [ServerInstance] -> [CompiledProc] -> ServersUsage
-serversUsageFromProcs instances procs = MS.execState (forM_ procs collectServers) M.empty
+serversUsageFromProcs instances procs = M.map S.toList $ MS.execState (forM_ procs collectServers) M.empty
     where
-        collectServers :: CompiledProc -> MS.State ServersUsage ()
-        collectServers CompiledProc{..} = forM_ cp_usedServersInstances $ addServer cp_name . serverNameFromInstance
+        collectServers :: CompiledProc -> MS.State (M.Map ServerName (S.Set ProcessName)) ()
+        collectServers CompiledProc{..} =
+            forM_ cp_usedServersInstances $ addServer cp_name . serverNameFromInstance
 
-        addServer :: ProcessName -> ServerName -> MS.State ServersUsage ()
-        addServer procN serverN = MS.modify $ M.insertWith (++) serverN [procN]
+        addServer :: ProcessName -> ServerName -> MS.State (M.Map ServerName (S.Set ProcessName)) ()
+        addServer procN serverN = MS.modify $ M.insertWith S.union serverN (S.singleton procN)
 
         serverNameFromInstance :: ServerInstanceName -> ServerName
         serverNameFromInstance name =